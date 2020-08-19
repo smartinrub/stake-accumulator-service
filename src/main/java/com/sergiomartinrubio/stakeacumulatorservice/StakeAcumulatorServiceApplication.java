ca32ebaa9219963eeca24d822fc1cf49646ffe77
Sergio Martin Rubio <econsergio@gmail.com>
2020-08-19 19:18:37 +0100

    Create project

---
+++ src/main/java/com/sergiomartinrubio/stakeacumulatorservice/StakeAcumulatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sergiomartinrubio.stakeacumulatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StakeAcumulatorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StakeAcumulatorServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sergiomartinrubio/stakeacumulatorservice/StakeAcumulatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiomartinrubio.stakeacumulatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StakeAcumulatorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
